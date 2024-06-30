b648d67a30738819926bbb311c3c4b141623c9b5
laianyau <laianyau@gmail.com>
2024-06-30 23:07:27 +0800

    branch: submit -answer 02\00~05

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -9,6 +9,12 @@
 // `factorial(2)` to return `2`, and so on.
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
+fn factorial(input:  u32) -> u32{
+    if input > 1 {
+        return  input * factorial(input-1)
+    }
+    else { return 1 }
+}
 
 #[cfg(test)]
 mod tests {
